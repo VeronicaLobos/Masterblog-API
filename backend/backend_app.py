0c4fc3f970f3cff40f69baed82b6c8102845e59b
VeronicaMG <33263432+VeronicaLobos@users.noreply.github.com>
2025-04-01 20:32:52 +0200

    Implemented and tested Search Endpoint

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,10 +1,11 @@
-from crypt import methods
-
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
+limiter = Limiter(app=app, key_func=get_remote_address)
 
 POSTS = [
     {"id": 1, "title": "First post", "content": "This is the first post."},
@@ -75,6 +76,25 @@ def update_post(post_id):
     return jsonify(post_to_update), 200
 
 
+@app.route('/api/posts/search', methods=['GET'])
+def search_posts():
+    """
+    Search for posts by title or content.
+    :return: A list of posts that match the search term.
+    """
+    # Get the search term from the query parameters
+    title = request.args.get('title', '')
+    content = request.args.get('content', '')
+    filtered_posts = []
+    # Filter the posts based on the search term
+    if title:
+        filtered_posts = [post for post in POSTS if title.lower()
+                          in post['title'].lower()]
+    elif content:
+        filtered_posts = [post for post in POSTS if content.lower()
+                          in post['content'].lower()]
+
+    return jsonify(filtered_posts), 200
 
 
 
